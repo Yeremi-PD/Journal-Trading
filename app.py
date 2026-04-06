cdb8ab46babe35b07faadd14c2ed36abd8b39005
Yeremi-PD <yeremipd353@gmail.com>
2026-04-06 18:37:00 -0400

    a

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ LBL_INPUT_BAL_Y = 0
 
 # --- BOTÓN DEL CALENDARIO 🗓️ (REFORZADO) ---
 BOTON_X = -170          # ¡Mueve el botón!
-BOTON_Y = 25         
+BOTON_Y = 27         
 BOTON_WIDTH = 45     
 BOTON_HEIGHT = 45    
 BOTON_ICON_SIZE = 22 
