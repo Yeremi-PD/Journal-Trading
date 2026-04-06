3d24a04e4d703dbc16abf54f9920e5ab81171419
Yeremi-PD <yeremipd353@gmail.com>
2026-04-06 16:45:03 -0400

    .

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
     }
     
     .balance-box { 
-        background: #2D3748; color: white; padding: 10px 20px; 
+        background: #2D3748; color: white; padding: 50px 50px; 
         border-radius: 10px; text-align: center; font-weight: 700; font-size: 22px;
         box-shadow: 0 4px 6px rgba(0,0,0,0.1);
     }
