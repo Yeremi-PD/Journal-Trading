4f5ac7de00b8da37ae5de54d218738e245f7703b
Yeremi-PD <yeremipd353@gmail.com>
2026-04-06 22:19:24 -0400

    a

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ LBL_INPUT_COLOR_O = "#FFFFFF"
 INPUT_BAL_W = "300"         # Ancho de la caja donde escribes
 INPUT_BAL_H = "399"          # Alto de la caja donde escribes
 INPUT_BAL_X = 0      
-INPUT_BAL_Y = 0      
+INPUT_BAL_Y = -50      
 INPUT_FONDO_C = "#FFFFFF"
 INPUT_FONDO_O = "#1A202C"
 
