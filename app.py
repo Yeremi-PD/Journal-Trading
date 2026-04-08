d9784f874b4e91096928551ef026bc88823e533b
Yeremi-PD <yeremipd353@gmail.com>
2026-04-08 15:36:52 -0400

    a

--- app.py
+++ app.py
@@ -474,10 +474,19 @@ if db_usuario[ctx_actual]["trades"]:
         mime="application/zip",
         use_container_width=True
     )
+
+
+
+
+
+    
 # BOTÓN DE SINCRONIZACIÓN FORZADA (CORTA EL CACHÉ Y OBLIGA A LEER EXCEL AL INSTANTE)
 if st.sidebar.button("🔄 Force Sync with Google Sheets", use_container_width=True):
     get_global_db.clear()
     st.rerun()
+
+
+
 
 
 st.sidebar.markdown("<br><br><br><div style='margin-top:30px;'></div>", unsafe_allow_html=True)
