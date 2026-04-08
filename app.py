2f48d7e161599d8b995be071090d4cbfe9c85bb9
Yeremi-PD <yeremipd353@gmail.com>
2026-04-07 23:35:00 -0400

    A

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ st.sidebar.markdown(f"### 👤 Mi Cuenta: {usuario}")
 
 st.session_state.dispositivo_actual = st.sidebar.radio("Current Design:", ["🖥️PC", "📱 Móvil"], index=0 if st.session_state.dispositivo_actual == "PC" else 1)
 
-if st.sidebar.button("💾 Save Design Settings", use_container_width=True):
+if st.sidebar.button("💾 Save Design Settings to Cloud", use_container_width=True):
     ctx_act = st.session_state.data_source_sel
     bal_act = db_usuario[ctx_act]["balance"]
     registrar_en_excel(usuario, db_global[usuario]["password"], ctx_act, datetime.now(), bal_act, 0.0, {}, db_global[usuario]["settings"]["PC"], db_global[usuario]["settings"]["Móvil"])
