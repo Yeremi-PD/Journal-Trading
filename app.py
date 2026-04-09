2dbb200d0bbf784125cedb7da368c7051b4f4055
Yeremi-PD <yeremipd353@gmail.com>
2026-04-08 21:36:49 -0400

    a

--- app.py
+++ app.py
@@ -837,11 +837,11 @@ with st.form(key=f"form_main_entry_{st.session_state.form_reset_key}", clear_on_
                 if cols_conf[idx % 3].checkbox(c_name, key=f"new_conf_{idx}"):
                     nuevo_conf.append(c_name)
                     
-            # TEXTOS
+            # TEXTOS (Cajas reducidas un 10%)
             st.markdown("<div style='margin-top: 10px;'></div>", unsafe_allow_html=True)
-            nuevo_razon = st.text_area("Reason For Trade", value='', height=50)
-            nuevo_corr = st.text_area("Corrections", value='', height=50)
-            nuevo_emo = st.text_area("Emotions", value='', height=50)
+            nuevo_razon = st.text_area("Reason For Trade", value='', height=45)
+            nuevo_corr = st.text_area("Corrections", value='', height=45)
+            nuevo_emo = st.text_area("Emotions", value='', height=45)
             
             # RISK (Con un poco más de separación entre las opciones)
             st.markdown("<div style='font-weight: 900; font-size: 14px; margin-top: 5px; margin-bottom: 0px;'>Risk</div>", unsafe_allow_html=True)
