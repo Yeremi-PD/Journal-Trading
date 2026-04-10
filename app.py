a839d84f5956b76fd4887cab83ac722831617545
Yeremi-PD <yeremipd353@gmail.com>
2026-04-10 15:25:22 -0400

    s

--- app.py
+++ app.py
@@ -671,7 +671,7 @@ st.sidebar.markdown("<div style='margin-top: 0px;'></div>", unsafe_allow_html=Tr
 
 # BOTÓN DE SINCRONIZACIÓN FORZADA
 st.sidebar.markdown("---")
-st.sidebar.markdown("### ↻ Sync with Google Sheets")
+st.sidebar.markdown("### Sync with Google Sheets")
 if st.sidebar.button("↻ Force Sync with Google Sheets", use_container_width=True):
     get_global_db.clear()
     st.rerun()
