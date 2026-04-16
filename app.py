3376a3c87f6a222e24be74a14af5d64c0e2b5d49
Yeremi-PD <yeremipd353@gmail.com>
2026-04-16 11:09:06 -0400

    a

--- app.py
+++ app.py
@@ -1011,7 +1011,7 @@ st.markdown(f"""
 
     .cell-win {{ border: 2px solid #00C897 !important; color: #00664F !important; background-color: #e6f9f4 !important;}}
     .cell-loss {{ border: 2px solid #FF4C4C !important; color: #9B1C1C !important; background-color: #ffeded !important;}}
-    .cell-be {{ border: 2px solid #A0AEC0 !important; color: #00C897 !important; background-color: #c2c2c2}}
+    .cell-be {{ border: 2px solid #00C897 !important; color: #00C897 !important; background-color: #c2c2c2}}
     .cell-empty {{ border: 1px solid {border_color} !important; background-color: {empty_cell_bg} !important;}}
 
     .modal-toggle:checked ~ .fs-modal {{ display: flex !important; }}
