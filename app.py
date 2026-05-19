78e5297c4dd4647945805766d8a6ed4869b83e3f
Yeremi-PD <yeremipd353@gmail.com>
2026-05-18 21:31:19 -0400

    A

--- app.py
+++ app.py
@@ -1314,12 +1314,16 @@ else: bal_inicial_abs = 25000.0
 meta_global = 1500 if bal_inicial_abs <= 35000 else (3000 if bal_inicial_abs <= 75000 else 6000)
 paso_cuenta, idx_pase = False, -1
 
-# 🚫 Bloqueamos la conversión a cuenta fondeada (PA) si estamos en Backtesting
-if not db_usuario[ctx].get("backtesting_mode", False):
-    for idx, tr in enumerate(_tc):
-        if (tr["balance_final"] - bal_inicial_abs) >= meta_global:
-            paso_cuenta, idx_pase = True, idx
-            break
+    # 🚫 Bloqueamos la conversión a cuenta fondeada (PA) si estamos en Backtesting
+    if not db_usuario[ctx].get("backtesting_mode", False):
+        for idx, tr in enumerate(_tc):
+            if (tr["balance_final"] - bal_inicial_abs) >= meta_global:
+                paso_cuenta, idx_pase = True, idx
+                break
+        
+        # 🟢 LÍNEAS NUEVAS: FORZAR PA SIEMPRE (Obliga a la cuenta a ser Fondeada)
+        paso_cuenta = True
+        idx_pase = len(_tc) - 1 if _tc else 0
 
 for idx, tr in enumerate(_tc): tr["is_pre_funded"] = (idx <= idx_pase)
 
