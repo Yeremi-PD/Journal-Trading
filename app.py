c40ba524896f85c6e9a2d5d21e71049f55e216f4
Yeremi-PD <yeremipd353@gmail.com>
2026-05-30 18:33:27 -0400

    a

--- app.py
+++ app.py
@@ -1206,7 +1206,7 @@ st.markdown(f"""
     [data-testid="stMainBlockContainer"],
     .main .block-container {{ 
         padding-top: 0px !important; 
-        margin-top: -90px !important; 
+        margin-top: -110px !important; 
         max-width: 100% !important;
     }}
 
